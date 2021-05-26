5aeaa5b2d9ff46b998464458a3b73937bc28f34f
David <74608966+dkelly7OSL@users.noreply.github.com>
2021-05-26 10:12:33 -0400

    Create __init__.py

---
+++ OSL_Calibration/__init__.py
@@ -0,0 +1,10 @@
+'''
+##################################### OPEN #####################################
+Filler file to flag this directory as importable.
+
+NOTE: This file is currently empty, but may change in the future.
+
+Last Update: 26 May 2021
+Updates: Created
+#################################### CLOSE #####################################
+'''
